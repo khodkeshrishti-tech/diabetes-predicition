11d6a5e31b191493b6aa30bd3da5bd638ef15e26
khodkeshrishti-tech <khodkeshrishti@gmail.com>
2026-03-26 17:02:44 +0530

    Added dataset and starter code

---
+++ model.py/model.py
@@ -0,0 +1,25 @@
+# ============================================================
+# LOGISTIC REGRESSION MODEL - DIABETES PREDICTION DATASET
+# ============================================================
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+# --- MEMBER 2: DATA CLEANING --- (Praveen)
+
+
+# --- MEMBER 3: VISUALIZATION / EDA --- (Harshvardhini)
+
+
+# --- MEMBER 4: FEATURE PREPARATION --- (Rohal)
+
+
+# --- MEMBER 5: SPLITTING & TRAINING --- (Mathdevru)
+
+
+# --- MEMBER 6: EVALUATION --- (Hemanth)
